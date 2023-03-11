367aad113f748dabbc069bd97d710b64f987d17b
Koudou Sugawara <ksugawara01@gmail.com>
2023-03-11 16:20:27 -0500

    completed exercises 3.15-3.17

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const Person = require('./models/person')
+const { findByIdAndUpdate } = require('./models/person')
 const app = express()
 
 app.use(express.static('build'))
@@ -11,15 +12,27 @@ app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
 
-//This is an object constructor
-//const Person = mongoose.model('Person', personSchema)
+//Error handler middleware
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+    
+    next(error)
+}
+//This must be the last loaded middleware
+app.use(errorHandler)  
+
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
 
 app.get('/info', (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people
+    console.log(Person.count())
+    response.send(`Phonebook has info for ${Person.count({}).then((count) => count)} people
     <br/>${new Date()}`)
     
 })
@@ -30,22 +43,39 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
+app.get('/api/persons/:id', (request, response, next) => {
 
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id != id)
 
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response) => {
+    const person = {
+        name: request.body.name,
+        number: request.body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
